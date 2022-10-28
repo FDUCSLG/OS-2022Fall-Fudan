647ca90c00ed2c68c95453d77a3edab7f78b6645
zzzly3 <1683994033@qq.com>
2022-10-28 18:01:48 +0800

    fix cpu.c

--- src/kernel/cpu.c
+++ src/kernel/cpu.c
@@ -95,7 +95,8 @@ void set_cpu_on() {
 }
 
 void set_cpu_off() {
-    _arch_disable_trap();
+    auto t = _arch_disable_trap();
+    (void)t;
     cpus[cpuid()].online = false;
     printk("CPU %d: stopped\n", cpuid());
 }
